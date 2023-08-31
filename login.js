00b9ca589474e99cd8a88f73839c220473ac78c0
hasan5323 <ibrahimhasan002@gmail.com>
2023-08-31 15:25:54 +0700

    alert login

--- login.js
+++ login.js
@@ -1,16 +1,8 @@
 function handleLogin() {
     let username=document.getElementById("username").value;
+    if (!username) {
+        alert("masukkan username");
+    }
     localStorage.setItem('user', username);
 }
-
-// userNameInpput.addEventListener('input', (e) => {
-// namaUser = e.target.value
-// })
-// let btn = document.querySelector(".button")
-// btn.addEventListener("click", () => {
-    
-
-
-// console.log(namaUser);
-// })
 
